137d90d8704f6f773479ff6c5dd170f7bcdf889e
王飞 <wangfei@starschina.com>
2016-05-08 20:02:39 +0800

    add-project

---
+++ 快速排序/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  快速排序
+//
+//  Created by 王飞 on 16/4/18.
+//  Copyright © 2016年 com.cube.cocoa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 快速排序/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  快速排序
+//
+//  Created by 王飞 on 16/4/18.
+//  Copyright © 2016年 com.cube.cocoa. All rights reserved.
+//
+
+#import "ViewController.h"
+//#include "<#header#>"
+//#include <math.h>
+#include <math.h>
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    int a[3] = [2,3,4];
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
